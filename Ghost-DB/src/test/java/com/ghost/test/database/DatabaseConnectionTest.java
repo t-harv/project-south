84fd990e47e0df3dee64e7e6d04df8a17eaa6b3b
toyan <toyan@toyan-VirtualBox>
2017-05-13 23:04:11 -0400

    Starting up this project again, just knocking off the dust!

--- Ghost-DB/src/test/java/com/ghost/test/database/DatabaseConnectionTest.java
+++ Ghost-DB/src/test/java/com/ghost/test/database/DatabaseConnectionTest.java
@@ -3,8 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class DatabaseConnectionTest {
 
