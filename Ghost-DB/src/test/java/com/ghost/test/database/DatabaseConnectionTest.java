776661522b22ede47af18d6a50d3fca0cd470bb8
toyan <toyan@toyan-VirtualBox>
2017-05-13 23:45:23 -0400

    changed from derby to oracle.  Updated DatabaseConnectionTest.java to test the switch.

--- Ghost-DB/src/test/java/com/ghost/test/database/DatabaseConnectionTest.java
+++ Ghost-DB/src/test/java/com/ghost/test/database/DatabaseConnectionTest.java
@@ -1,55 +1,43 @@
 package com.ghost.test.database;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import org.apache.commons.dbcp.BasicDataSource;
 
 
 public class DatabaseConnectionTest {
 
 	public static void main(String[] args) {
 
-		Connection con = null;
-		Statement st = null;
-
-		String url = "jdbc:derby://localhost:1527/GhostWriterDB;user=APP;create=true";
-		
+		/**
+		 * update
+		 */
 		try {
+			String username = "toyan";
+			String password = "turpleP420";
+			String url = "jdbc:oracle:thin:@//localhost:1521/xe";
+			String port = "1521";
+			String DBName = "toyan";
 
-			//System.setProperty("derby.system.home", "C:/Development/derby/db-derby-10.11.1.1-bin/bin");
+			String oracleDriver = "oracle.jdbc.driver.OracleDriver";
 
-			con = DriverManager.getConnection(url);
-			st = con.createStatement();
-			st.executeUpdate("CREATE TABLE CARS4(ID INT PRIMARY KEY,"
-					+ "NAME VARCHAR(30), PRICE INT)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(1, 'Audi', 52642)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(2, 'Mercedes', 57127)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(3, 'Skoda', 9000)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(4, 'Volvo', 29000)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(5, 'Bentley', 350000)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(6, 'Citroen', 21000)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(7, 'Hummer', 41400)");
-			st.executeUpdate("INSERT INTO CARS3 VALUES(8, 'Volkswagen', 21600)");
-			//DriverManager.getConnection("jdbc:derby:;shutdown=true");
-
-		} catch (SQLException ex) {
-
-			ex.printStackTrace();
-
-		} finally {
-
-			try {
-
-				if (st != null) {
-					st.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-
-			} catch (SQLException ex) {
-				ex.printStackTrace();
-			}
+			BasicDataSource ds = new BasicDataSource();
+			
+			ds.setUsername(username);
+			ds.setPassword(password);
+			ds.setUrl(url);
+			ds.setDriverClassName(oracleDriver);
+			ds.setMaxWait(-1L);
+			ds.setTestWhileIdle(true);
+			ds.setTimeBetweenEvictionRunsMillis(60000);
+			ds.setValidationQuery("select * from dual");
+			
+			Connection conn = ds.getConnection();
+			
+			System.out.println(conn.isClosed());
+			conn.close();
+			System.out.println(conn.isClosed());
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 	}
 }
