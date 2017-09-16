ea21488aa2d818edcedc9c8498768cb285a81682
t-harv <toyan.harvey@gmail.com>
2017-09-15 22:49:41 -0400

    Class files that shouldn't be added!

---
+++ Ghost-WS/src/main/java/org/Ghost/WS/App.java
@@ -0,0 +1,13 @@
+package org.Ghost.WS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
