6f8356f976b0067b44d366547137dc86d06dafb0
t-harv <toyan.harvey@gmail.com>
2017-09-16 00:05:45 -0400

    Straggler!  Forgot to check in.

--- Ghost-Services/src/main/java/ghost/service/CharacterService.java
+++ Ghost-Services/src/main/java/ghost/service/CharacterService.java
@@ -1,5 +1,34 @@
 package ghost.service;
 
+import java.util.List;
+
+import com.ghost.writing.character.Character;
+
 public class CharacterService {
+
+	public List<Character> getAllCharacters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Character getCharacter(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Character addCharacter(Character character) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Character updateCharacter(Character character) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Character removeCharacter(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
