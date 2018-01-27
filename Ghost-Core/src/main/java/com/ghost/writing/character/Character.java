99cec2a222b871467ade0c2a36ecd36b5e0c20c7
toyan <toyan@toyan-VirtualBox>
2018-01-27 00:13:52 -0500

    Removed the abstract from character.  Added comments.  The plan is to do alot of updates to this class so that we can leverage it in the hibernate example project.

--- Ghost-Core/src/main/java/com/ghost/writing/character/Character.java
+++ Ghost-Core/src/main/java/com/ghost/writing/character/Character.java
@@ -1,6 +1,17 @@
 package com.ghost.writing.character;
 
-public abstract class Character {
+/**
+ * 
+ * @author toyan
+ * 
+ * Going to use this class as a helper to learn complex hibernate funtions.
+ * 
+ * 1.  Complex Key out of ID.  The key will reside in the character table (will not be embedded (seperate table))
+ * 2.  Embedded table - I need to use composition to merge another object into this character object.  This will allow me to use hibernate to create a sub table out of the second object.
+ * 		Example would be if i added address object to this character object.  There will be a character table for the character object and an address table for the address object.
+ *
+ */
+public class Character {
 	private long id;
 	private String firstName;
 	private String lastName;
