6798bbf39d7fa036bae4576d4a91d37757cf8eae
toyan <toyan@toyan-VirtualBox>
2018-05-20 02:08:28 -0400

    Unit Test update

--- Ghost-Core/test/com/ghost/writing/character/domainobject/impl/CharacterProfileImplTest.java
+++ Ghost-Core/test/com/ghost/writing/character/domainobject/impl/CharacterProfileImplTest.java
@@ -16,51 +16,51 @@ import com.ghost.writing.character.domainobject.SpiritualCharacteristics;
 import com.ghost.writing.components.Nationality.Race;
 
 public class CharacterProfileImplTest {
-	private CharacterProfile joyce;
-	private CharacterProfile langston;
+	private CharacterProfile jala;
+	private CharacterProfile babyL;
 	
 
 	@Before
-	public void setupJoyce() {
-		joyce = new CharacterProfileImpl();
+	public void setupJala() {
+		jala = new CharacterProfileImpl();
 		
-		((CharacterProfileImpl)joyce).setBirthOrder(3);
-		List<String> joyceChildren = new ArrayList<String>();
-		joyceChildren.add("Langston Harvey");
-		((CharacterProfileImpl)joyce).setChildren(joyceChildren); //Langston & Toyce
-		((CharacterProfileImpl)joyce).setCurrentAddress("14306 Sunset View Dr, Bowie MD 20720");
-		((CharacterProfileImpl)joyce).setFirstName("Joyce");
-		List<String> joyceGrandChildren = new ArrayList<String>();
-		((CharacterProfileImpl)joyce).setGrandChildren(joyceGrandChildren);
-		List<String> joyceGrandParents = new ArrayList<String>();
-		joyceGrandParents.add("Grandma JJ");
-		joyceGrandParents.add("Grandpa JJ");
-		((CharacterProfileImpl)joyce).setGrandParents(joyceGrandParents);
-		((CharacterProfileImpl)joyce).setHomeTown("Japan Okinawa");
-		((CharacterProfileImpl)joyce).setIncome("400k");
-		((CharacterProfileImpl)joyce).setLastName("Harvey");
-		((CharacterProfileImpl)joyce).setName("Joyce Minksy Harvey");
-		((CharacterProfileImpl)joyce).setNat(Race.BLACK);
-		((CharacterProfileImpl)joyce).setOccupation("Software Developer");
-		List<String> joyceOtherAddresses = new ArrayList<String>();				
-		((CharacterProfileImpl)joyce).setOtherAddresses(joyceOtherAddresses);
-		((CharacterProfileImpl)joyce).setSalary("400k");
-		List<String> joyceSiblings = new ArrayList<String>();
-		joyceSiblings.add("DeeDee Paterson");
-		joyceSiblings.add("Winston Curry");
-		((CharacterProfileImpl)joyce).setSiblings(joyceSiblings);
-		List<String> joyceSignificantOthers = new ArrayList<String>();
-		joyceSignificantOthers.add("Toyan Harvey");
-		((CharacterProfileImpl)joyce).setSignificantOthers(joyceSignificantOthers);
-		List<String> joyceSkills = new ArrayList<String>();
-		joyceSkills.add("Mommy Skills");
-		((CharacterProfileImpl)joyce).setSkills(joyceSkills);
-		((CharacterProfileImpl)joyce).setSocioeconomicLevelAsAChild("Middle Class");
-		((CharacterProfileImpl)joyce).setSocioeconomicLevelAsAnAdult("Middle Class");
-		((CharacterProfileImpl)joyce).setSpouse("Toyan Harvey");
-		List<String> joyceTalents = new ArrayList<String>();
-		joyceTalents.add("Tap Dancing");
-		((CharacterProfileImpl)joyce).setTalents(joyceTalents);
+		((CharacterProfileImpl)jala).setBirthOrder(3);
+		List<String> jalaChildren = new ArrayList<String>();
+		jalaChildren.add("BabyL Haley");
+		((CharacterProfileImpl)jala).setChildren(jalaChildren); //BabyL & Toyce
+		((CharacterProfileImpl)jala).setCurrentAddress("14306 Sunset View Dr, Bowie MD 20720");
+		((CharacterProfileImpl)jala).setFirstName("Jala");
+		List<String> jalaGrandChildren = new ArrayList<String>();
+		((CharacterProfileImpl)jala).setGrandChildren(jalaGrandChildren);
+		List<String> jalaGrandParents = new ArrayList<String>();
+		jalaGrandParents.add("Grandma JJ");
+		jalaGrandParents.add("Grandpa JJ");
+		((CharacterProfileImpl)jala).setGrandParents(jalaGrandParents);
+		((CharacterProfileImpl)jala).setHomeTown("Japan Okinawa");
+		((CharacterProfileImpl)jala).setIncome("400k");
+		((CharacterProfileImpl)jala).setLastName("Haley");
+		((CharacterProfileImpl)jala).setName("Jala Minksy Haley");
+		((CharacterProfileImpl)jala).setNat(Race.BLACK);
+		((CharacterProfileImpl)jala).setOccupation("Software Developer");
+		List<String> jalaOtherAddresses = new ArrayList<String>();				
+		((CharacterProfileImpl)jala).setOtherAddresses(jalaOtherAddresses);
+		((CharacterProfileImpl)jala).setSalary("400k");
+		List<String> jalaSiblings = new ArrayList<String>();
+		jalaSiblings.add("DeeDee Paterson");
+		jalaSiblings.add("Winston Curry");
+		((CharacterProfileImpl)jala).setSiblings(jalaSiblings);
+		List<String> jalaSignificantOthers = new ArrayList<String>();
+		jalaSignificantOthers.add("Toyan Haley");
+		((CharacterProfileImpl)jala).setSignificantOthers(jalaSignificantOthers);
+		List<String> jalaSkills = new ArrayList<String>();
+		jalaSkills.add("Mommy Skills");
+		((CharacterProfileImpl)jala).setSkills(jalaSkills);
+		((CharacterProfileImpl)jala).setSocioeconomicLevelAsAChild("Middle Class");
+		((CharacterProfileImpl)jala).setSocioeconomicLevelAsAnAdult("Middle Class");
+		((CharacterProfileImpl)jala).setSpouse("Toyan Haley");
+		List<String> jalaTalents = new ArrayList<String>();
+		jalaTalents.add("Tap Dancing");
+		((CharacterProfileImpl)jala).setTalents(jalaTalents);
 		
 		
 		
@@ -71,10 +71,10 @@ public class CharacterProfileImplTest {
 		((SpiritualCharacteristicsImpl)sc).setBelieve(true);
 		((SpiritualCharacteristicsImpl)sc).setRole("Everything");
 		((SpiritualCharacteristicsImpl)sc).setSpiritual(true);
-		List<String> joyceSpiritualBeliefs = new ArrayList<String>();
-		joyceSpiritualBeliefs.add("Guidance");
-		((SpiritualCharacteristicsImpl)sc).setSpiritualBeliefs(joyceSpiritualBeliefs);
-		((CharacterProfileImpl)joyce).setSc(sc);
+		List<String> jalaSpiritualBeliefs = new ArrayList<String>();
+		jalaSpiritualBeliefs.add("Guidance");
+		((SpiritualCharacteristicsImpl)sc).setSpiritualBeliefs(jalaSpiritualBeliefs);
+		((CharacterProfileImpl)jala).setSc(sc);
 		
 		
 		
@@ -82,29 +82,29 @@ public class CharacterProfileImplTest {
 		PhysicalCharacteristics pc = new PhysicalCharacteristicsImpl();
 		((PhysicalCharacteristicsImpl)pc).setBestQuality("Smart");
 		((PhysicalCharacteristicsImpl)pc).setContactLenses(false);
-		List<String> joyceDisabilities = new ArrayList<String>();
-		((PhysicalCharacteristicsImpl)pc).setDisabilities(joyceDisabilities);
-		List<String> joyceDistinguishingFeatures = new ArrayList<String>();
-		joyceDistinguishingFeatures.add("Nose Mole");
-		joyceDistinguishingFeatures.add("Tounge Birthmark");
-		((PhysicalCharacteristicsImpl)pc).setDistinguishingFeatures(joyceDistinguishingFeatures);
+		List<String> jalaDisabilities = new ArrayList<String>();
+		((PhysicalCharacteristicsImpl)pc).setDisabilities(jalaDisabilities);
+		List<String> jalaDistinguishingFeatures = new ArrayList<String>();
+		jalaDistinguishingFeatures.add("Nose Mole");
+		jalaDistinguishingFeatures.add("Tounge Birthmark");
+		((PhysicalCharacteristicsImpl)pc).setDistinguishingFeatures(jalaDistinguishingFeatures);
 		((PhysicalCharacteristicsImpl)pc).setDressCode("Casual");
 		((PhysicalCharacteristicsImpl)pc).setEyeColor("Brown");
 		((PhysicalCharacteristicsImpl)pc).setFavoriteSaying("Kick Rocks");
 		((PhysicalCharacteristicsImpl)pc).setGlasse(true);
 		((PhysicalCharacteristicsImpl)pc).setGreatestFlaw("Temper");
-		List<String> joyceHabits = new ArrayList<String>();
-		joyceHabits.add("Worry");
-		joyceHabits.add("Reading articles");
-		((PhysicalCharacteristicsImpl)pc).setHabits(joyceHabits);
+		List<String> jalaHabits = new ArrayList<String>();
+		jalaHabits.add("Worry");
+		jalaHabits.add("Reading articles");
+		((PhysicalCharacteristicsImpl)pc).setHabits(jalaHabits);
 		((PhysicalCharacteristicsImpl)pc).setHairColor("Black");
-		List<String> joyceHealth = new ArrayList<String>();
-		joyceHealth.add("Healthy");
-		((PhysicalCharacteristicsImpl)pc).setHealth(joyceHealth);
+		List<String> jalaHealth = new ArrayList<String>();
+		jalaHealth.add("Healthy");
+		((PhysicalCharacteristicsImpl)pc).setHealth(jalaHealth);
 		((PhysicalCharacteristicsImpl)pc).setHight(6);
-		List<String> joyceHobbies = new ArrayList<String>();
-		joyceHobbies.add("Reading");
-		((PhysicalCharacteristicsImpl)pc).setHobbies(joyceHobbies);
+		List<String> jalaHobbies = new ArrayList<String>();
+		jalaHobbies.add("Reading");
+		((PhysicalCharacteristicsImpl)pc).setHobbies(jalaHobbies);
 		((PhysicalCharacteristicsImpl)pc).setMannerisms("Well");
 		((PhysicalCharacteristicsImpl)pc).setRace(Race.BLACK);
 		((PhysicalCharacteristicsImpl)pc).setShapeOfFace("Oval");
@@ -112,34 +112,34 @@ public class CharacterProfileImplTest {
 		((PhysicalCharacteristicsImpl)pc).setSpeachPatterns("Perfact Grammar and English");
 		((PhysicalCharacteristicsImpl)pc).setStyle("What's popular");
 		((PhysicalCharacteristicsImpl)pc).setWeight(185);
-		((CharacterProfileImpl)joyce).setPc(pc);
+		((CharacterProfileImpl)jala).setPc(pc);
 		
 		
 		
 //		Intellectual Characteristics
 		IntellectualCharacteristics ic = new IntellectualCharacteristicsImpl();
-		List<String> joyceEducation = new ArrayList<String>();
-		joyceEducation.add("Bacholers");
-		joyceEducation.add("Masters");
-		((IntellectualCharacteristicsImpl)ic).setEducation(joyceEducation);
+		List<String> jalaEducation = new ArrayList<String>();
+		jalaEducation.add("Bacholers");
+		jalaEducation.add("Masters");
+		((IntellectualCharacteristicsImpl)ic).setEducation(jalaEducation);
 		((IntellectualCharacteristicsImpl)ic).setEmbarrassment("Comments towards self");
 		((IntellectualCharacteristicsImpl)ic).setEmotional(true);
 		((IntellectualCharacteristicsImpl)ic).setIntelligenceLevel("High");
-		List<String> joyceLearningExperiences = new ArrayList<String>();
-		joyceLearningExperiences.add("Life");
-		((IntellectualCharacteristicsImpl)ic).setLearningExperiences(joyceLearningExperiences);
-		List<String> joyceLongTermGoals = new ArrayList<String>();
-		joyceLongTermGoals.add("Christianity");
-		((IntellectualCharacteristicsImpl)ic).setLongTermGoals(joyceLongTermGoals);
-		List<String> joyceMentalIllnesses = new ArrayList<String>();
-		((IntellectualCharacteristicsImpl)ic).setMentalIllnesses(joyceMentalIllnesses);
+		List<String> jalaLearningExperiences = new ArrayList<String>();
+		jalaLearningExperiences.add("Life");
+		((IntellectualCharacteristicsImpl)ic).setLearningExperiences(jalaLearningExperiences);
+		List<String> jalaLongTermGoals = new ArrayList<String>();
+		jalaLongTermGoals.add("Christianity");
+		((IntellectualCharacteristicsImpl)ic).setLongTermGoals(jalaLongTermGoals);
+		List<String> jalaMentalIllnesses = new ArrayList<String>();
+		((IntellectualCharacteristicsImpl)ic).setMentalIllnesses(jalaMentalIllnesses);
 		((IntellectualCharacteristicsImpl)ic).setPerception("Positive");
 		((IntellectualCharacteristicsImpl)ic).setSelfConfident(true);
 		((IntellectualCharacteristicsImpl)ic).setSelfViewPoint("High");
-		List<String> joyceShortTermGoals = new ArrayList<String>();
-		joyceShortTermGoals.add("Another Baby");
-		((IntellectualCharacteristicsImpl)ic).setShortTermGoals(joyceShortTermGoals);
-		((CharacterProfileImpl)joyce).setIc(ic);
+		List<String> jalaShortTermGoals = new ArrayList<String>();
+		jalaShortTermGoals.add("Another Baby");
+		((IntellectualCharacteristicsImpl)ic).setShortTermGoals(jalaShortTermGoals);
+		((CharacterProfileImpl)jala).setIc(ic);
 		
 		//Emotional Characteristics
 		EmotionalCharacteristics ec = new EmotionalCharacteristicsImpl();
@@ -150,21 +150,21 @@ public class CharacterProfileImplTest {
 		((EmotionalCharacteristicsImpl)ec).setHappy("50/50");
 		((EmotionalCharacteristicsImpl)ec).setJudgmental(true);
 		((EmotionalCharacteristicsImpl)ec).setLossManagement("Unknown");
-		List<String> joyceMotivations = new ArrayList<String>();
-		joyceMotivations.add("Langston");
-		joyceMotivations.add("God");
-		((EmotionalCharacteristicsImpl)ec).setMotivations(joyceMotivations);
+		List<String> jalaMotivations = new ArrayList<String>();
+		jalaMotivations.add("BabyL");
+		jalaMotivations.add("God");
+		((EmotionalCharacteristicsImpl)ec).setMotivations(jalaMotivations);
 		((EmotionalCharacteristicsImpl)ec).setPolite(true);
 		((EmotionalCharacteristicsImpl)ec).setRude(false);
 		((EmotionalCharacteristicsImpl)ec).setSadManagement("Not good");
-		List<String> joyceStrengths = new ArrayList<String>();
-		joyceStrengths.add("Herself");
-		((EmotionalCharacteristicsImpl)ec).setStrengths(joyceStrengths);
+		List<String> jalaStrengths = new ArrayList<String>();
+		jalaStrengths.add("Herself");
+		((EmotionalCharacteristicsImpl)ec).setStrengths(jalaStrengths);
 		((EmotionalCharacteristicsImpl)ec).setWantOutOfLife("Happiness");
-		List<String> joyceWeaknesses = new ArrayList<String>();
-		joyceWeaknesses.add("Food!");
-		((EmotionalCharacteristicsImpl)ec).setWeaknesses(joyceWeaknesses);
-		((CharacterProfileImpl)joyce).setEc(ec);
+		List<String> jalaWeaknesses = new ArrayList<String>();
+		jalaWeaknesses.add("Food!");
+		((EmotionalCharacteristicsImpl)ec).setWeaknesses(jalaWeaknesses);
+		((CharacterProfileImpl)jala).setEc(ec);
 		
 		
 		//character arch
@@ -172,43 +172,43 @@ public class CharacterProfileImplTest {
 		((CharacterArchImpl)ca).setCharacterRole("Unknown");
 		((CharacterArchImpl)ca).setFinale("Unknown");
 		((CharacterArchImpl)ca).setFirstAppearance("2013, Washington DC");
-		List<CharacterProfile> joyceRelationships = new ArrayList<CharacterProfile>();
-		((CharacterArchImpl)ca).setRelationships(joyceRelationships);
-		((CharacterProfileImpl)joyce).setCa(ca);
+		List<CharacterProfile> jalaRelationships = new ArrayList<CharacterProfile>();
+		((CharacterArchImpl)ca).setRelationships(jalaRelationships);
+		((CharacterProfileImpl)jala).setCa(ca);
 	}
 	
 	@Test
 	public void compareTest() {
-		Assert.assertEquals(((CharacterProfileImpl)getJoyce()).getFirstName(), "Joyce");
+		Assert.assertEquals(((CharacterProfileImpl)getJala()).getFirstName(), "Jala");
 		
 	}
 
 	/**
-	 * @return the joyce
+	 * @return the jala
 	 */
-	public CharacterProfile getJoyce() {
-		return joyce;
+	public CharacterProfile getJala() {
+		return jala;
 	}
 
 	/**
-	 * @param joyce the joyce to set
+	 * @param jala the jala to set
 	 */
-	public void setJoyce(CharacterProfile joyce) {
-		joyce = joyce;
+	public void setJala(CharacterProfile jala) {
+		jala = jala;
 	}
 
 	/**
-	 * @return the langston
+	 * @return the babyL
 	 */
-	public CharacterProfile getLangston() {
-		return langston;
+	public CharacterProfile getBabyL() {
+		return babyL;
 	}
 
 	/**
-	 * @param langston the langston to set
+	 * @param babyL the babyL to set
 	 */
-	public void setLangston(CharacterProfile langston) {
-		langston = langston;
+	public void setBabyL(CharacterProfile babyL) {
+		babyL = babyL;
 	}
 	
 
